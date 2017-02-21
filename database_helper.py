4de6eb19f50c6ae89554644520f1182f168613eb
Fredrik Håkansson <freha309@student.liu.se>
2017-02-21 14:26:24 +0100

    Initial files with Bootstrap

---
+++ database_helper.py
@@ -0,0 +1,119 @@
+# coding=utf-8
+from flask import Flask, g
+from sqlite3 import IntegrityError
+import sqlite3
+import os
+
+__author__ = 'Fredrik Håkansson (freha309)'
+
+app = Flask(__name__)
+
+DATABASE = path = os.path.dirname(os.path.abspath(__file__)) + '/database.db'
+# DATABASE = 'C:\Users\Fredrik\Documents\git\lab2_tddd97\database.db'
+
+
+# Initializes the database and are called at server startup
+# Reads the queries stored in the 'database.schema' file
+def init_database():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('database.schema', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+        print "Database initialized"
+
+
+# Returns the database object to execute queries
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g.database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+# Simplifies selecting data from Database
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+# Adds a user to the database
+def add_user(email, password, firstname, familyname, gender, city, country):
+    db = get_db()
+    try:
+        result = db.execute("INSERT INTO Users VALUES (?,?,?,?,?,?,?);", (email, password, firstname, familyname, gender, city, country))
+        db.commit()
+    except IntegrityError:
+        return None
+
+    return result
+
+
+# Returns a sqlite3.Row with a user matching the email
+def find_user(email):
+    result = query_db("SELECT * FROM Users WHERE email = ?",
+                      (email,), one=True)
+    if result is None:
+        return None
+    else:
+        return result
+
+
+# Sets new password for the provided user
+def update_password(email, password):
+    db = get_db()
+    result = db.execute("UPDATE Users SET password = ? WHERE email = ?", (password, email))
+    db.commit()
+    return result
+
+
+# Fetches the user email corresponding to the provided token
+def get_email_with_token(token):
+    sql = "SELECT email FROM LoggedInUsers WHERE token=?"
+    result = query_db(sql, (token,), one=True)
+    return result
+
+
+# Fetches user messages belonging to the provided email
+def get_messages(email):
+    result = query_db("SELECT id, author, message FROM Messages WHERE user=?", (email,))
+    return result
+
+
+# Adds a new message to a user (receiver)
+def add_message(receiver, author, message):
+    db = get_db()
+    result = db.execute("INSERT INTO Messages (user, author, message) VALUES (?, ?, ?)", (receiver, author, message))
+    db.commit()
+    return result
+
+
+# Adds token to database with corresponding user's email
+def add_token(email, token):
+    db = get_db()
+    try:
+        res = db.execute("INSERT INTO LoggedInUsers VALUES (?, ?);", (email, token))
+        db.commit()
+    except IntegrityError:
+        res = db.execute("UPDATE LoggedInUsers SET token=? WHERE email=?", (token, email))
+        db.commit()
+    return res
+
+
+# Removes token from database
+def remove_token(token):
+    db = get_db()
+    res = db.execute("DELETE FROM LoggedInUsers WHERE token=?", (token,))
+    db.commit()
+    return res
+
---
+++ static/js/client.js
@@ -0,0 +1,268 @@
+var webSocket = new WebSocket("ws://localhost:5000/socket");
+webSocket.onmessage = function (event) {
+    if (event.data == "logout") {
+        sessionStorage.removeItem("token");
+        displayView();
+    }
+};
+
+
+displayView = function(){
+    // the code required to display a view
+    var callback = function (response) {
+        var view = null;
+        if (getToken() != null && response.success == true) {
+            view = document.getElementById("profileview");
+            document.getElementById("placeholder").innerHTML = view.innerHTML;
+
+            getUserData();
+            // Get the element with id="defaultOpen" and click on it
+            document.getElementById("defaultOpen").click();
+        }
+        else {
+            view = document.getElementById("welcomeview");
+            document.getElementById("placeholder").innerHTML = view.innerHTML;
+        }
+    };
+    httpRequest("GET", "/getUserData?token="+getToken(),null ,callback);
+    return false;
+};
+window.onload = function(){
+    displayView();
+};
+
+
+function validateSignUp() {
+    const PASSWORD_LENGTH = 8;
+    var form = document.forms["signUp-form"];
+    var email = form["email"].value;
+    var password = form["password"].value;
+    var password_repeat = form["password_repeat"].value;
+    var alertText = document.getElementById("signUp-alert");
+
+    if (password.length < PASSWORD_LENGTH || password_repeat.length < PASSWORD_LENGTH) {
+        alertText.innerHTML = "Password too short!";
+    } else {
+        if (password != password_repeat) {
+            alertText.innerHTML = "Password doesn't match!";
+        } else {
+            var signUpData = {
+                email: email,
+                password: password,
+                firstname: form["firstname"].value,
+                familyname: form["familyname"].value,
+                gender: form["gender"].value,
+                city: form["city"].value,
+                country: form["country"].value
+            };
+            var data = new FormData();
+            for (var key in signUpData) {
+                data.append(key, signUpData[key]);
+            }
+
+            var callback = function (response) {
+                if (response.success == true) {
+                    sessionStorage.setItem("token", response.data.token);
+                    displayView();
+                } else {
+                    if (response.message == "Not authenticated")
+                        displayView();
+                    alertText.innerHTML = response.message;
+                }
+            };
+            httpRequest("POST", "/signup", data, callback);
+        }
+    }
+    return false;
+}
+
+function login() {
+    var form = document.forms["login-form"];
+    var alertText = document.getElementById("login-alert");
+    var token = "";
+    var data = new FormData();
+    data.append("email", form["email"].value);
+    data.append("password", form["password"].value);
+
+    var callback = function (response) {
+        if(response.success == true) {
+            token = response.data.token;
+            console.log("Token: " + token);
+            sessionStorage.setItem("token", token);
+            var socketMessage = {"message": "login", "token": token};
+            webSocket.send(JSON.stringify(socketMessage));
+            displayView();
+        } else {
+            sessionStorage.removeItem("token");
+            console.log("Failed: " + response.message);
+            alertText.innerHTML = response.message;
+        }
+    };
+    httpRequest("POST", "/login", data, callback);
+
+    return false;
+}
+
+function logout() {
+    var callback = function (response) {
+        if (response.success == true) {
+            sessionStorage.removeItem("token");
+            displayView();
+        } else {
+            if (response.message == "Not authenticated")
+                displayView();
+            console.log(response.message);
+        }
+    };
+    httpRequest("POST", "/logout?token="+getToken(), null, callback);
+    return false;
+}
+
+function getToken() {
+    return sessionStorage.getItem("token");
+}
+
+
+function changePassword() {
+    var form = document.forms["changePasswordForm"];
+    var old_password = form["old"].value;
+    var new_password_1 = form["new1"].value;
+    var new_password_2 = form["new2"].value;
+    var alertText = document.getElementById("changeAlert");
+
+    if (new_password_1.length < 8 || new_password_2.length < 8) {
+        alertText.innerHTML = "Invalid length of password!"
+    } else {
+        if (new_password_1 == new_password_2) {
+            var data = new FormData();
+            data.append("old", old_password);
+            data.append("new1", new_password_1);
+
+            var callback = function (response) {
+                alertText.innerHTML = response.message;
+            };
+            httpRequest("POST", "/changePassword?token="+getToken(), data, callback);
+        } else {
+            alertText.innerHTML = "Password doesn't match!";
+        }
+    }
+    return false;
+}
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+function getUserData() {
+    var callback = function (response) {
+        var user = response.data;
+        document.getElementById("nameField").innerHTML = user.firstname;
+        document.getElementById("familyField").innerHTML = user.familyname;
+        document.getElementById("home-emailField").innerHTML = user.email;
+        document.getElementById("genderField").innerHTML = user.gender;
+        document.getElementById("cityField").innerHTML = user.city;
+        document.getElementById("countryField").innerHTML = user.country;
+        updateComments('home');
+    };
+    httpRequest("GET", "/getUserData?token="+getToken(), null, callback);
+    return false;
+}
+
+function postComment(page) {
+    var message = document.getElementById(page + "-comment-input").value;
+    var email = document.getElementById(page + "-emailField").innerHTML;
+    var data = new FormData();
+    data.append("user", email);
+    data.append("message", message);
+    var callback = function(response) {
+        if (response.success == true) {
+            updateComments(page, email);
+        } else {
+            if (response.message == "Not authenticated")
+                displayView();
+            console.log(response.message);
+        }
+    };
+    httpRequest("POST", "/postMessage?token="+getToken(), data, callback);
+    return false;
+}
+
+function updateComments(page, email) {
+    if (typeof email === "undefined") {
+        email = document.getElementById(page + "-emailField").innerHTML;
+    }
+
+    var callback = function (response) {
+        if (response.success) {
+
+            var comments = response.data;
+            var commentHolder =  document.getElementById(page + "-comment-holder");
+            var content = "";
+            for (var i = 0; i < comments.length; i++) {
+                content += "<h4 class='comment-header'>" + comments[i].author + ":</h4>";
+                content += "<p class='comment-body'>" + comments[i].message + "</p>";
+                content += "<hr>"
+            }
+
+            commentHolder.innerHTML = content;
+        } else {
+            if (response.message = "Not authenticated")
+                displayView()
+        }
+    };
+    httpRequest("GET", "/getUserMessages/"+email+"?token="+getToken(), null, callback);
+    return false;
+}
+
+function browseUser() {
+    var alertText = document.getElementById("browseAlert");
+    var input = document.getElementById("searchUserText").value;
+    var callback = function(response) {
+        if (response.success == true) {
+            alertText.innerHTML = "";
+            var user = response.data;
+            document.getElementById("browse-nameField").innerHTML = user.firstname;
+            document.getElementById("browse-familyField").innerHTML = user.familyname;
+            document.getElementById("browse-emailField").innerHTML = user.email;
+            document.getElementById("browse-genderField").innerHTML = user.gender;
+            document.getElementById("browse-cityField").innerHTML = user.city;
+            document.getElementById("browse-countryField").innerHTML = user.country;
+            document.getElementById("browseInfo").style.display = "inline";
+            updateComments('browse');
+        } else {
+            if (response.message == "Not authenticated")
+                displayView();
+            document.getElementById("browseInfo").style.display = "none";
+            alertText.innerHTML = response.message;
+        }
+    };
+    httpRequest("GET", "/getUserData/"+input+"?token="+getToken(), null, callback);
+    return false;
+}
+
+function httpRequest(method, url, data, callback) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+            callback(response);
+        }
+    };
+    xhttp.open(method, "http://localhost:5000" + url, true);
+    if (data != null)
+        xhttp.send(data);
+    else
+        xhttp.send();
+}
+
